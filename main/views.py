861e06c2018d92a9b41e936532116d01f083f0bd
Bishal Dahal <bishaldahal4777@gmail.com>
2025-10-17 10:53:31 +0545

    new views

--- main/views.py
+++ main/views.py
@@ -1,3 +1,18 @@
+import requests
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    rate = None
+
+    if 'from_currency' in request.GET and 'to_currency' in request.GET:
+        from_currency = request.GET['from_currency']
+        to_currency = request.GET['to_currency']
+
+        url = f"https://api.exchangerate.host/convert?from={from_currency}&to={to_currency}"
+        response = requests.get(url)
+        data = response.json()
+
+        if data.get('success'):
+            rate = data['result']
+
+    return render(request, 'main/index.html', {'rate': rate})
